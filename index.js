2faa817f151cb0dce34cedd2c2dc6a3667c8d7aa
davie kong <iscooleye@163.com>
2019-07-07 12:04:03 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var crypto = require('crypto');  //引入加密模块
+var config = require('./config');//引入配置文件
+var http = require('http');
+
+var app = express();
+
+
+app.get('/wx', function (req, res) {
+
+    //1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
+    var signature = req.query.signature,//微信加密签名
+        timestamp = req.query.timestamp,//时间戳
+        nonce = req.query.nonce,//随机数
+        echostr = req.query.echostr;//随机字符串
+
+    //2.将token、timestamp、nonce三个参数进行字典序排序
+    console.log(config.token)
+    var array = [config.token, timestamp, nonce];
+    array.sort();
+
+    //3.将三个参数字符串拼接成一个字符串进行sha1加密
+    var tempStr = array.join('');
+    const hashCode = crypto.createHash('sha1'); //创建加密类型 
+    var resultCode = hashCode.update(tempStr, 'utf8').digest('hex'); //对传入的字符串进行加密
+
+    //4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+    if (resultCode === signature) {
+        res.send(echostr);
+    } else {
+        res.send('mismatch');
+    }
+});
+
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
